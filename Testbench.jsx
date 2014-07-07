d899ee47dfdf109264706788138f884080d399f0
Dylan Winter <dylanwinter@gmail.com>
2014-07-06 21:35:40 -0400

    Post-Crash Madness!

--- Testbench.jsx
+++ Testbench.jsx
@@ -6,3 +6,5 @@ var curComp = app.project.activeItem;
 var selLayer = app.project.selectedLayers[0];
 
 // GO WILD
+
+alert(app.project.item(1).layer("Light Targets").property("Effects").property("Target 1")("Layer"));
